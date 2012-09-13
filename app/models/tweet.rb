038459f7bc8ce3da1c431ab959f7ac181cec90cb
Joe Merante <merante.joe@gmail.com>
2012-09-12 19:54:07 -0700

    switch pull_tweets method back to since_id from max_id

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ActiveRecord::Base
 	attr_accessible :tweet_id, :content, :user_name, :twitter_name, :tweeted_at, :user_image
 
 	def self.pull_tweets
-	  @search = Twitter.search("#cfaATX -rt", :max_id => 244467320376918016)
+	  @search = Twitter.search("#cfaATX -rt", since_id: Tweet.maximum('tweet_id'))
 	  @search.results.to_enum.each do |tweet|
 	    unless exists?(tweet_id: tweet.id)
 	      create!(
